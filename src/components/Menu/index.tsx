c5f297aaa30b74b0f171bad1a7f62d49c52357d4
samuelmarnatti <samuelmarnatti@gmail.com>
2025-09-13 21:50:09 -0300

    recurso de troca de tema

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,23 +1,51 @@
-import { HouseIcon, HistoryIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { HouseIcon, HistoryIcon, SettingsIcon, SunIcon, MoonIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useState, useEffect } from 'react';
 
+type AvailablesThemes = 'dark' | 'light';
 export function Menu() {
+
+  const [theme, setTheme] = useState<AvailablesThemes>(() => {
+    const storageTheme = localStorage.getItem('theme') as AvailablesThemes || 'dark';
+    return storageTheme;
+  });
+
+  function handleThemeChange(event) {
+    event.preventDefault();
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    })
+  }
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />
+  };
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href='#'>
+      <a className={styles.menuLink} href='#' aria-label='Ir para a Home'
+        title='Ir para a Home'>
         <HouseIcon />
       </a>
 
-      <a className={styles.menuLink} href='#'>
+      <a className={styles.menuLink} href='#' aria-label='Ver Histórico'
+        title='Ver Histórico'>
         <HistoryIcon />
       </a>
 
-      <a className={styles.menuLink} href='#'>
+      <a className={styles.menuLink} href='#' aria-label='Configurações'
+        title='Configurações'>
         <SettingsIcon />
       </a>
 
-      <a className={styles.menuLink} href='#'>
-        <SunIcon />
+      <a className={styles.menuLink} href='#' aria-label='Mudar Tema'
+        title='Mudar Tema' onClick={handleThemeChange}>
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
